8dbc50bd55711d1585df5b4c73d92456dcea8721
bhayek12@gmail.com <bhayek12@gmail.com>
2019-01-21 13:33:51 -0700

    Add portfolio component

---
+++ src/Components/Portfolio/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Portfolio.css';
+
+class Portfolio extends Component {
+    constructor() {
+        super()
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Portfolio;
