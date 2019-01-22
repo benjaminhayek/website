cd7dd6691a048b0c87d6189807e014329e4953eb
bhayek12@gmail.com <bhayek12@gmail.com>
2019-01-21 17:04:14 -0700

    Add world of flags to porfolio

--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import MTMain from '../utils/screen1.png';
 import MTLogin from '../utils/screen3.png';
+import WOFHome from '../utils/wof-home.png';
+import WOFLogin from '../utils/wof-login.png';
 import './Portfolio.css';
 
 class Portfolio extends Component {
@@ -14,6 +16,7 @@ class Portfolio extends Component {
                     <h1 className='portfolio-title'>PORTFOLIO</h1>
                     <h2 className='description'>- See my projects below. All projects were done by me or in partnership with my fellow classmates.</h2>
                 </div>
+                <hr/>
                 <div className='movie-tracker'>
                     <h1 className='mt-title'>Movie Tracker</h1>
                     <div className='mt-info'>
@@ -40,6 +43,36 @@ class Portfolio extends Component {
                             <img className='mt-login' src={MTLogin} />
                         </div>
                     </div>
+                </div>
+                <hr/>
+                <div className='world-of-flags'>
+                    <h1 className='mt-title'>World of Flags</h1>
+                    <div className='mt-info'>
+                        <div className='mt-left'>
+                                <h2 className='description-title'>DESCRIPTION:</h2>
+                                <h3 className='mt-description'>World of flags is a mobile app designed to help users teach themselves the flags of the world. Its underlying goal is to stimulate users’ curiosity about the world by familiarizing them with different countries’ flags.When interacting with World of Flags, users are shown a randomly generated flag from one of the world’s 193 countries and then can choose between 4 country options. If they get stuck, users can request up to 2 hints about the country whose flag they are guessing. These hints consist of facts about the country or an image of the country’s outline. By viewing these hints, users can grow to associate different flags with facts about their countries. As users correctly guess different flags, they will accumulate points. In this way, World of Flags aims to make learning about the world a fun and enjoyable process.</h3>
+                                <h2 className='tech-used-title'>TECHNOLOGIES:</h2>
+                                <div className='tech-holder'>
+                                    <a className='tech-links' href='https://reactjs.org/'>React</a>
+                                    <a className='tech-links' href='https://redux.js.org/'>Redux</a>
+                                    <a className='tech-links' href='https://reacttraining.com/react-router/'>React-Router</a>
+                                    <a className='tech-links' href='https://nodejs.org/en/'>NodeJS</a>
+                                    <a className='tech-links' href='https://airbnb.io/enzyme/'>Enzyme</a>
+                                    <a className='tech-links' href='https://jestjs.io/'>Jest</a>
+                                    <a className='tech-links' href='https://tavis-ci.org/'>Travis CI</a>
+                                    <a className='tech-links' href='http://www.nightmarejs.org/'>NightmareJS</a>
+                                </div>
+                                <h2 className='deployed-links'>LINKS:</h2>
+                                <div className='deployed-container'>
+                                    <a href='https://world-of-flags.herokuapp.com/' className='heroku'>Link to Deployed App</a>
+                                    <a href='https://github.com/benjaminhayek/world-of-flags' className='heroku'>Link to GitHub</a>
+                                </div>
+                            </div>
+                            <div className='mt-right'>
+                                <img className='wof-home' src={WOFHome} />
+                                <img className='wof-login' src={WOFLogin} />
+                            </div>
+                        </div>
                 </div>
             </div>
         )
