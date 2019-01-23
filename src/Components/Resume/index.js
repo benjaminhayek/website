2db809d6bea0f20c2080c2e18b10a13345250754
bhayek12@gmail.com <bhayek12@gmail.com>
2019-01-23 09:43:51 -0700

    Capitolize resume header

--- src/Components/Resume/index.js
+++ src/Components/Resume/index.js
@@ -6,7 +6,7 @@ import './Resume.css';
 const Resume = () => {
     return (
         <div className='resume'>
-            <h1 className='resume-title'>Resume</h1>
+            <h1 className='resume-title'>RESUME</h1>
             <a className='pdf' href={PDF}>- Click Here For My PDF</a>
             <div className='resume-container'>
                 <img className='bh-resume' src={BHResume} />
