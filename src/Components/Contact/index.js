097653dfdf2563c44c688a46d02f98da651119c7
bhayek12@gmail.com <bhayek12@gmail.com>
2019-01-23 10:35:54 -0700

    Style contact page

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -4,22 +4,25 @@ import './Contact.css';
 const Contact = () => {
     return (
         <div className='contact'>
-            <h1 className='contact-header'>CONTACT ME</h1>
-            <h3 className='availability'>- I am available to hire and open to any ideas of cooperation.</h3>
-            <div className='contact-card'>
-                <dl className='dl'>
-                    <dt className='dt'>Email:</dt>
-                    <dd className='dd'><a className='dd-email' href='bhayek12@gmail.com'><i class="fas fa-envelope-square"></i> bhayek12@gmail.com</a></dd>
-                    <dt></dt>
-                    <dd></dd>
-                    <dt className='dt'>Linked-In:</dt>
-                    <dd className='dd'><a className='dd-linkedin' href='https://www.linkedin.com/in/benjamin-hayek/'><i class="fab fa-linkedin"></i> benjamin-hayek</a></dd>
-                    <dt className='dt'>GitHub:</dt>
-                    <dd className='dd'><a className='dd-github' href='https://github.com/benjaminhayek'><i class="fab fa-github-square"></i> benjaminhayek</a></dd>
-                    <dt className='dt'>Twitter:</dt>
-                    <dd className='dd'><a className='dd-twitter' href='https://twitter.com/hayek_ben'><i class="fab fa-twitter-square"></i> hayek_ben</a></dd>
-                </dl>
+            <div className='contact-top'>
+                <h1 className='contact-header'>CONTACT ME</h1>
+                <h3 className='availability'>- I am available to hire and open to any ideas of cooperation.</h3>
+                <div className='contact-card'>
+                    <dl className='dl'>
+                        <dt className='dt dt-email'>Email:</dt>
+                        <dd className='dd dd-email'><a className='dd-email a-link' href='bhayek12@gmail.com'><i class="fas fa-envelope-square"></i> bhayek12@gmail.com</a></dd>
+                        <dt></dt>
+                        <dd></dd>
+                        <dt className='dt'>Linked-In:</dt>
+                        <dd className='dd'><a className='dd-linkedin a-link' href='https://www.linkedin.com/in/benjamin-hayek/'><i class="fab fa-linkedin"></i> benjamin-hayek</a></dd>
+                        <dt className='dt'>GitHub:</dt>
+                        <dd className='dd'><a className='dd-github a-link' href='https://github.com/benjaminhayek'><i class="fab fa-github-square"></i> benjaminhayek</a></dd>
+                        <dt className='dt'>Twitter:</dt>
+                        <dd className='dd'><a className='dd-twitter a-link' href='https://twitter.com/hayek_ben'><i class="fab fa-twitter-square"></i> hayek_ben</a></dd>
+                    </dl>
+                </div>
             </div>
+            <div className='contact-bottom'></div>
         </div>
     )
 }
