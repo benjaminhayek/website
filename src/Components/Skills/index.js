5e890d62eb7856d5bb7810351e281bfaeb0a1650
bhayek12@gmail.com <bhayek12@gmail.com>
2019-01-23 10:47:39 -0700

    Add skills container

---
+++ src/Components/Skills/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Skills.css';
+
+const Skills = () => {
+    return (
+        <div className='skills'>
+            <h1 className='skills-title'>SKILLS</h1>
+        </div>
+    )
+}
+
+export default Skills;
