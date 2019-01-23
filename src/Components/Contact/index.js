0bc2e6ced353327071bc3e47cebeb3af5bfccfc2
bhayek12@gmail.com <bhayek12@gmail.com>
2019-01-23 09:35:56 -0700

    Add contact component

---
+++ src/Components/Contact/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Contact.css';
+
+const Contact = () => {
+    return (
+        <div className='contact'>
+            <h1 className='contact-header'>CONTACT</h1>
+        </div>
+    )
+}
+
+export default Contact;
