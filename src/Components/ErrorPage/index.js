6af45373c01608bca96c80eaa8770b329f77b589
bhayek12@gmail.com <bhayek12@gmail.com>
2019-02-12 10:11:03 -0700

    Add error message

---
+++ src/Components/ErrorPage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ErrorPage.css'
+
+const ErrorPage = () => {
+    return(
+        <div>
+            <h1 className='error-title'>Sorry that is not a valid address :(</h1>
+        </div>
+    )
+}
+
+export default ErrorPage;
